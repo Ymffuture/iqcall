422a8bf5abfe059b791d16f67e9df789dfd5a28c
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2024-09-22 01:55:32 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 },
         }}
       >
-        <body className={`${inter.className} bg-dark-1`}>
+        <body className={`${inter.className} bg-dark-2`}>
           <Toaster />
           {children}
         </body>
