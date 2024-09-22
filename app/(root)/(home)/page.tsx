97fcacbe7f596c7268a3de8ccf63a514ec581c35
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2024-09-22 02:48:09 +0200

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,37 +1,23 @@
-"use client";
 
-import { useEffect, useState } from 'react';
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
-  const [time, setTime] = useState('');
-  const [date, setDate] = useState('');
+  const now = new Date();
 
-  const updateDateTime = () => {
-    const now = new Date();
-    const formattedTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-    const formattedDate = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(now);
-    
-    setTime(formattedTime);
-    setDate(formattedDate);
-  };
-
-  useEffect(() => {
-    updateDateTime(); // Set initial time and date
-    const timer = setInterval(updateDateTime, 60000); // Update every minute
-    return () => clearInterval(timer); // Cleanup on unmount
-  }, []);
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal bg-opacity-80 bg-gray-800">
-            Upcoming Meeting at: {time}
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 14:40 PM
+  
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p className="text-lg font-medium text-gray-300 lg:text-2xl">{date}</p>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
